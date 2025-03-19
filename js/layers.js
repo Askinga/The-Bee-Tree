bf51251f89f33583b458bb92624bf9a8483a30bd
Glitch (substantial-pastoral-wormhole) <none>
2025-03-19 23:30:49 +0000

    ☂️🍐 Checkpoint ./js/layers.js:63181522/18031

--- js/layers.js
+++ js/layers.js
@@ -9,17 +9,17 @@ addLayer("f", {
     };
   },
   passiveGeneration() {
-    let p = new OmegaNum(1)
-    if(hasUpgrade('f',14)) p = p.times(6.5)
-    if(hasUpgrade('f',15)) p = p.times(2)
-    if(hasUpgrade('f',23)) p = p.times(upgradeEffect('f', 23))
-    if(hasUpgrade('f',24)) p = p.times(10)
-    if(hasUpgrade('f',31)) p = p.times(1000)
-    if(hasUpgrade('f',42)) p = p.times(1e6)
-    if(hasUpgrade('f',43)) p = p.times(upgradeEffect('f', 43))
-    if(hasUpgrade('f',44)) p = p.times(upgradeEffect('f', 44))
-    if(hasUpgrade('f',52)) p = p.times(upgradeEffect('f', 52))
-    return p
+    let p = new OmegaNum(1);
+    if (hasUpgrade("f", 14)) p = p.times(6.5);
+    if (hasUpgrade("f", 15)) p = p.times(2);
+    if (hasUpgrade("f", 23)) p = p.times(upgradeEffect("f", 23));
+    if (hasUpgrade("f", 24)) p = p.times(10);
+    if (hasUpgrade("f", 31)) p = p.times(1000);
+    if (hasUpgrade("f", 42)) p = p.times(1e6);
+    if (hasUpgrade("f", 43)) p = p.times(upgradeEffect("f", 43));
+    if (hasUpgrade("f", 44)) p = p.times(upgradeEffect("f", 44));
+    if (hasUpgrade("f", 52)) p = p.times(upgradeEffect("f", 52));
+    return p;
   },
   color: "#86AEF3",
   requires: new OmegaNum(10), // Can be a function that takes requirement increases into account
@@ -29,24 +29,24 @@ addLayer("f", {
     return player.points;
   }, // Get the current amount of baseResource
   type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-  exponent: 1e-10000000000000000000000000, // Prestige currency exponent
+  exponent: 0, // Prestige currency exponent
   gainMult() {
     // Calculate the multiplier for main currency from bonuses
     mult = new OmegaNum(1);
-    if(hasUpgrade('f',14)) mult = mult.times(3.5)
-    if(hasUpgrade('f',15)) mult = mult.times(5)
-    if(hasUpgrade('f',21)) mult = mult.times(upgradeEffect('f', 21))
-    if(hasUpgrade('f',22)) mult = mult.times(upgradeEffect('f', 22))
-    if(hasUpgrade('f',24)) mult = mult.times(10)
-    if(hasUpgrade('f',25)) mult = mult.times(upgradeEffect('f', 25))
-    if(hasUpgrade('f',31)) mult = mult.times(2.5)
-    if(hasUpgrade('f',32)) mult = mult.times(upgradeEffect('f', 32))
-    if(hasUpgrade('f',33)) mult = mult.times(upgradeEffect('f', 33))
-    if(hasUpgrade('f',34)) mult = mult.times(upgradeEffect('f', 34))
-    if(hasUpgrade('f',35)) mult = mult.times(upgradeEffect('f', 35))
-    if(hasUpgrade('f',41)) mult = mult.times(upgradeEffect('f', 41))
-    if(hasUpgrade('f',42)) mult = mult.times(1e6)
-    if(hasUpgrade('f',51)) mult = mult.times(upgradeEffect('f', 51))
+    if (hasUpgrade("f", 14)) mult = mult.times(3.5);
+    if (hasUpgrade("f", 15)) mult = mult.times(5);
+    if (hasUpgrade("f", 21)) mult = mult.times(upgradeEffect("f", 21));
+    if (hasUpgrade("f", 22)) mult = mult.times(upgradeEffect("f", 22));
+    if (hasUpgrade("f", 24)) mult = mult.times(10);
+    if (hasUpgrade("f", 25)) mult = mult.times(upgradeEffect("f", 25));
+    if (hasUpgrade("f", 31)) mult = mult.times(2.5);
+    if (hasUpgrade("f", 32)) mult = mult.times(upgradeEffect("f", 32));
+    if (hasUpgrade("f", 33)) mult = mult.times(upgradeEffect("f", 33));
+    if (hasUpgrade("f", 34)) mult = mult.times(upgradeEffect("f", 34));
+    if (hasUpgrade("f", 35)) mult = mult.times(upgradeEffect("f", 35));
+    if (hasUpgrade("f", 41)) mult = mult.times(upgradeEffect("f", 41));
+    if (hasUpgrade("f", 42)) mult = mult.times(1e6);
+    if (hasUpgrade("f", 51)) mult = mult.times(upgradeEffect("f", 51));
     return mult;
   },
   gainExp() {
@@ -86,8 +86,8 @@ addLayer("f", {
 
       cost: new OmegaNum("100"),
       effect() {
-        let tetPow = new OmegaNum(0.1)
-        if(hasUpgrade('f', 45)) tetPow = tetPow.times(upgradeEffect('f',45))
+        let tetPow = new OmegaNum(0.1);
+        if (hasUpgrade("f", 45)) tetPow = tetPow.times(upgradeEffect("f", 45));
         return new OmegaNum(10).tetrate(player.f.points.add(1).pow(tetPow));
       },
 
@@ -95,26 +95,21 @@ addLayer("f", {
         return "10^^" + format(upgradeEffect("f", 13));
       },
     },
-      14: {
+    14: {
+      title: "Faster",
 
-	    title: "Faster",
+      description: "Make flower generation x6.5 quicker and x3.5 Flowers.",
 
-	    description: "Make flower generation x6.5 quicker and x3.5 Flowers.",
-
-	    cost: new OmegaNum("500")
-
+      cost: new OmegaNum("500"),
     },
     15: {
+      title: "Even Faster",
 
-	    title: "Even Faster",
+      description: "Make flower generation x2 quicker and x5 Flowers.",
 
-	    description: "Make flower generation x2 quicker and x5 Flowers.",
-
-	    cost: new OmegaNum("6000")
-
+      cost: new OmegaNum("6000"),
     },
     21: {
-
       title: "More Flowers",
 
       description: "Boost Flowers based on Flowers",
@@ -122,19 +117,14 @@ addLayer("f", {
       cost: new OmegaNum("50000"),
 
       effect() {
-
         return player.f.points.add(1).pow(0.2);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 21));
-
       },
-  },
-  22: {
-
+    },
+    22: {
       title: "Even More Flowers",
 
       description: "Boost Flowers based on Flowers again but weaker",
@@ -142,19 +132,14 @@ addLayer("f", {
       cost: new OmegaNum("5e5"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(1.25);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 22));
-
       },
     },
     23: {
-
       title: "More Flower Generation",
 
       description: "Boost Flowers generation based on Flowers",
@@ -162,29 +147,21 @@ addLayer("f", {
       cost: new OmegaNum("2.5e7"),
 
       effect() {
-
-        return player.f.points.add(1).pow(0.1);
-
+        return player.f.points.add(1).log(10).add(1).pow(1.25);
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 23));
-
       },
-
     },
     24: {
+      title: "Faster^2",
 
-	    title: "Faster^2",
+      description: "Make flower generation x10 quicker and x10 Flowers.",
 
-	    description: "Make flower generation x10 quicker and x10 Flowers.",
-
-	    cost: new OmegaNum("1e9")
-
+      cost: new OmegaNum("1e9"),
     },
     25: {
-
       title: "Even More Flowers 2",
 
       description: "Boost Flowers based on Flowers again",
@@ -192,29 +169,21 @@ addLayer("f", {
       cost: new OmegaNum("2e10"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(2);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 25));
-
       },
-
     },
     31: {
+      title: "The Holy Bees",
 
-	    title: "The Holy Bees",
+      description: "Make flower generation x1000 quicker and x2.5 Flowers.",
 
-	    description: "Make flower generation x1000 quicker and x2.5 Flowers.",
-
-	    cost: new OmegaNum("1e15")
-
+      cost: new OmegaNum("1e15"),
     },
     32: {
-
       title: "Even More Flowers 3",
 
       description: "Boost Flowers based on Flowers again",
@@ -222,20 +191,14 @@ addLayer("f", {
       cost: new OmegaNum("1e22"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(5);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 32));
-
       },
-
     },
     33: {
-
       title: "Even More Flowers 4",
 
       description: "Boost Flowers based on Flowers again",
@@ -243,20 +206,14 @@ addLayer("f", {
       cost: new OmegaNum("1e27"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(3);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 33));
-
       },
-
     },
     34: {
-
       title: "Even More Flowers 5",
 
       description: "Boost Flowers based on Flowers again",
@@ -264,20 +221,14 @@ addLayer("f", {
       cost: new OmegaNum("2e37"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(1.5);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 34));
-
       },
-
     },
     35: {
-
       title: "Even More Flowers 6",
 
       description: "Boost Flowers based on Flowers again",
@@ -285,20 +236,14 @@ addLayer("f", {
       cost: new OmegaNum("1.5e43"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(1.25);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 35));
-
       },
-
     },
     41: {
-
       title: "Even More Flowers Final",
 
       description: "Boost Flowers based on Flowers again",
@@ -306,29 +251,21 @@ addLayer("f", {
       cost: new OmegaNum("1e49"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(4);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 41));
-
       },
-
     },
     42: {
+      title: "The God Bees",
 
-	    title: "The God Bees",
+      description: "Make flower generation x1e6 quicker and x1e6 Flowers.",
 
-	    description: "Make flower generation x1e6 quicker and x1e6 Flowers.",
-
-	    cost: new OmegaNum("1e70")
-
+      cost: new OmegaNum("1e70"),
     },
     43: {
-
       title: "Even More Flower Generation",
 
       description: "Boost Flowers generation based on Flowers again",
@@ -336,20 +273,14 @@ addLayer("f", {
       cost: new OmegaNum("1e90"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(2);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 43));
-
       },
-
     },
     44: {
-
       title: "Even More Flower Generation 2",
 
       description: "Boost Flowers generation based on Flowers again",
@@ -357,20 +288,14 @@ addLayer("f", {
       cost: new OmegaNum("1e98"),
 
       effect() {
-
         return player.f.points.add(1).log(10).add(1).pow(1.5);
-
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 44));
-
       },
-
     },
     45: {
-
       title: "Get past the FF1e10 range",
 
       description: "Boost Upgrade 3 based on Flowers",
@@ -378,20 +303,14 @@ addLayer("f", {
       cost: new OmegaNum("1.5e103"),
 
       effect() {
-
         return player.f.points.add(1).pow(0.00065);
-
       },
 
       effectDisplay() {
-
         return "Tetraton Power x" + format(upgradeEffect("f", 45));
-
       },
-
     },
     51: {
-
       title: "Yet More Flowers",
 
       description: "Boost Flowers based on Flowers",
@@ -399,20 +318,14 @@ addLayer("f", {
       cost: new OmegaNum("2e103"),
 
       effect() {
-
-        return player.f.points.add(1).pow(0.1);
-
+        return player.f.points.add(1).log(10).add(1).pow(1.1);
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 51));
-
       },
-
     },
     52: {
-
       title: "Even More Flower Generation 3",
 
       description: "Boost Flowers generation based on Flowers again",
@@ -420,17 +333,12 @@ addLayer("f", {
       cost: new OmegaNum("1e124"),
 
       effect() {
-
-        return player.f.points.log(10).pow(3.5);
-
+        return player.f.points.add(1).log(10).add(1).pow(3.5);
       },
 
       effectDisplay() {
-
         return "x" + format(upgradeEffect("f", 52));
-
       },
-
     },
-   },
+  },
 });
